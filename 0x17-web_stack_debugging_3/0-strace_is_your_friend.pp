656419c793878f9108716f2026618acbce531890
giwabasit17 <giwabasit17@gmail.com>
2023-11-09 07:24:16 -0800

    web_stack_debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,3 @@
-code for webstuck debugging`: 
 # Fixes a wordpress site running on apache2
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
